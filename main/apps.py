ddd6b0bcfaee5e803bf6d4548741f9077a81bb43
Yashaswini-penumarthi <x24262404@student.ncirl.ie>
2026-03-06 12:57:56 +0000

    pylint changes

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,8 @@
+"""Django apps"""
 from django.apps import AppConfig
 
 
 class MainConfig(AppConfig):
+    """Django main app"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
